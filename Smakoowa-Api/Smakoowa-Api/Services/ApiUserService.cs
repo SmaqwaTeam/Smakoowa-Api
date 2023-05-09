2b48d78f939f0c32654beca3404d553eae8dcd1a
bw <bartwol111@gmail.com>
2023-05-09 22:08:59 +0200

    GetCurrentUserId touchup

--- Smakoowa-Api/Smakoowa-Api/Services/ApiUserService.cs
+++ Smakoowa-Api/Smakoowa-Api/Services/ApiUserService.cs
@@ -1,10 +1,32 @@
-﻿namespace Smakoowa_Api.Services
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Smakoowa_Api.Services
 {
     public class ApiUserService : IApiUserService
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ApiUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public int GetCurrentUserId()
         {
-            return int.Parse(Program.configuration["CurrentUserId"]);
+            if (System.Diagnostics.Process.GetCurrentProcess().ProcessName.StartsWith("testhost"))
+            {
+                return 1;
+            }
+
+            var authHeader = _httpContextAccessor.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+            var token = authHeader.Substring("Bearer ".Length);
+
+            var handler = new JwtSecurityTokenHandler();
+            var claims = handler.ReadJwtToken(token).Claims;
+            var userIdClaim = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+            return int.Parse(userIdClaim.Value);
         }
     }
 }
