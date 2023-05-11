863ad587b5c97477be588b41155e85e70ec35268
bw <bartwol111@gmail.com>
2023-05-11 16:42:30 +0200

    cleanup

--- Smakoowa-Api/Smakoowa-Api/Services/Interfaces/MapperServices/ICommentMapperService.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace Smakoowa_Api.Services.Interfaces.MapperServices
-//{
-//    public interface ICommentMapperService
-//    {
-//        public RecipeComment MapCreateRecipeCommentRequestDto(RecipeCommentRequestDto recipeCommentRequestDto, int recipeId);
-//        public CommentReply MapCreateCommentReplyRequestDto(CommentReplyRequestDto commentReplyRequestDto, int commentId);
-//        public Comment MapEditCommentRequestDto(CommentRequestDto commentRequestDto, Comment editedComment);
-//    }
-//}
--- Smakoowa-Api/Smakoowa-Api/Services/Interfaces/ValidatorServices/ICommentValidatorService.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace Smakoowa_Api.Services.Interfaces.ValidatorServices
-//{
-//    public interface ICommentValidatorService
-//    {
-//        public Task<ServiceResponse> ValidateRecipeCommentRequestDto(RecipeCommentRequestDto recipeCommentRequestDto, int recipeId);
-//        public Task<ServiceResponse> ValidateCommentReplyRequestDto(CommentReplyRequestDto commentReplyRequestDto, int commentId);
-//    }
-//}
