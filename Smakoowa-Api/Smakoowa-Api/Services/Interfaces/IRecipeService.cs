ed5941dead4ca187edea2a83ec2c7c2e0ae0fbe9
bw <bartwol111@gmail.com>
2023-05-13 13:36:45 +0200

    added RecipeGetterService

--- Smakoowa-Api/Smakoowa-Api/Services/Interfaces/IRecipeService.cs
+++ Smakoowa-Api/Smakoowa-Api/Services/Interfaces/IRecipeService.cs
@@ -2,15 +2,5 @@
 {
     public interface IRecipeService : ICreatorService<RecipeRequestDto>, IEditorService<RecipeRequestDto>, IDeleterService
     {
-        public Task<ServiceResponse> GetById(int id);
-        public Task<ServiceResponse> GetAll(GetRecipeParameters? getRecipeParameters);
-        public Task<ServiceResponse> GetByIdDetailed(int recipeId);
-        public Task<ServiceResponse> GetCurrentUsersRecipes(GetRecipeParameters? getRecipeParameters);
-        public Task<ServiceResponse> GetRecipesByTagIds(List<int> tagIds, GetRecipeParameters? getRecipeParameters);
-        public Task<ServiceResponse> GetRecipesByCategoryId(int categoryId, GetRecipeParameters? getRecipeParameters);
-        public Task<ServiceResponse> SearchRecipesByName(string querry, GetRecipeParameters? getRecipeParameters);
-        public Task<ServiceResponse> GetRecipiesByLikedTags(GetRecipeParameters? getRecipeParameters);
-        public Task<ServiceResponse> GetLikedRecipies(GetRecipeParameters? getRecipeParameters);
-        public Task<ServiceResponse> GetUserRecipies(int userId, GetRecipeParameters? getRecipeParameters);
     }
 }
